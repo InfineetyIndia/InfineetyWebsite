95f9b7a5643a5b4ccae1a348a9cc3b26eb10c0c8
mukesh jha <mukrjha@gmail.com>
2021-05-18 09:17:37 +0530

    upload static file

--- infinity/settings.py
+++ infinity/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-vae&ls!#v5ld$u4c)few#1s%ez@lyd#32o)8himf-p%-+k*p2!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-13-232-49-253.ap-south-1.compute.amazonaws.com','0.0.0.0','https://infineety.com']
+ALLOWED_HOSTS = ['ec2-13-232-49-253.ap-south-1.compute.amazonaws.com','0.0.0.0','https://infineety.com','3.108.45.28']
 #ALLOWED_HOSTS = ["*"]
 
 
@@ -133,7 +134,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+#STATIC_URL = '/static/'
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
