fe688e156c1b0f82906bf68483d367e1890b6d76
mukesh jha <mukrjha@gmail.com>
2021-05-31 09:16:55 +0530

    payment

--- infinity/urls.py
+++ infinity/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('user/', include('users.urls', namespace='users')),
-    # path('', include('payments.urls'))
+    path('payment', include('payments.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
